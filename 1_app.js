0a7ccc10548114c2dabfbff5c7468f8c5a46546e
Pushpajit Sarkar <23f1002179@ds.study.iitm.ac.in>
2026-02-20 21:40:07 +0530

    #4 - Data Binding

--- 1_app.js
+++ 1_app.js
@@ -5,7 +5,10 @@ new Vue({
       // Define the reactive data properties
       data: {
             name: "Deep",
-            job: "Student"
+            job: "Student",
+            courseName: "Vue JS 2 Tutorial",
+            website: "https://medium.com/@hyejunglim/summary-of-vue-js2-tutorial-by-net-ninja-part-1-of-3-402fec3a77ff",
+            websiteTag: '<a href="https://youtube.com/playlist?list=PL4cUxeGkcC9gQcYgjhBoeQH7wiAyZNrYa&si=mEyDhxBnE9z_ClCE" target="_blank">That YT course we learning from.</a>'
       },
       methods: {
             greet: function (time) {
