712124bf8040d41de49e8ab15c4869d12405402d
Pushpajit Sarkar <23f1002179@ds.study.iitm.ac.in>
2026-02-20 20:45:55 +0530

    #2 - The Vue Instance

---
+++ 1_app.js
@@ -0,0 +1,10 @@
+// Create a new Vue instance to control the app
+new Vue({
+      // Mount the Vue instance to the element with id "vue-controlled"
+      el: "#vue-controlled",
+      // Define the reactive data properties
+      data: {
+            name: "Deep",
+            job: "Student"
+      }
+})
