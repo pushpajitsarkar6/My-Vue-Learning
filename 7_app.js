38a88ab32c08ba314f7409f4505720dfa43f5e52
Pushpajit Sarkar <23f1002179@ds.study.iitm.ac.in>
2026-02-21 12:05:45 +0530

    #7 - Keyboard Events

---
+++ 7_app.js
@@ -0,0 +1,14 @@
+new Vue({
+      el: "#Joining-Vue",
+      data: {
+
+      },
+      methods: {
+            logName: function () {
+                  console.log("You entered your name");
+            },
+            logAge: function () {
+                  console.log("You entered your age");
+            }
+      }
+})
