f7294d173fe57a257c54dcc7dab933c6c81e6645
Pushpajit Sarkar <23f1002179@ds.study.iitm.ac.in>
2026-02-22 23:23:44 +0530

    #12 - Looping with v-for

---
+++ 12_app.js
@@ -0,0 +1,18 @@
+new Vue({
+      el: '#Vue-app-12',
+      data: {
+            name: 'Robert Downey Jr.',
+            characters: ['Dr. Doom', 'Tony Stark', 'Sherlock Holmes', 'Charlie Chaplin'],   //Array
+            friends: [                                                                      //Array of objects
+                  { name: 'Chris Evans', age: 40, movies: ['Captain America', 'Snowpiercer', 'Gifted'] },
+                  { name: 'Chris Hemsworth', age: 38, movies: ['Thor', 'Extraction', 'Rush'] },
+                  { name: 'Scarlett Johansson', age: 36, movies: ['Black Widow', 'Marriage Story', 'Lucy'] },
+            ]
+      },
+      methods: {
+
+      },
+      computed: {
+
+      }
+})
